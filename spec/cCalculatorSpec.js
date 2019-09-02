a3c216ddb1652dd8838ce4ca03ec5e80ad8e297b
kenneth Mahlatse <kennethmahlatsemokhethi@gmail.com>
2019-09-02 15:50:51 +0200

    Update cCalculatorSpec.js
    Deleted the commented line

--- spec/cCalculatorSpec.js
+++ spec/cCalculatorSpec.js
@@ -1,32 +1,23 @@
 //Testing the functions for addintion of two and multiply numbers
 describe("Class:Addition operator,two arguments", () => {
   it("Summing up two numbers.test1", () => {
-    
-    // const objCalculator = new cCalculator();
     var sum = objCalculator.sumNumbers(0,0);
     expect(sum).toBe(0);
   });
 
   it("Summing up two numbers.test2", () => {
-    
-    // const objCalculator = new cCalculator();
     var sum = objCalculator.sumNumbers(-1,-1);
     expect(sum).toBe(-2);
   });
 
   it("Summing up two numbers.test3", () => {
-    
-    // const objCalculator = new cCalculator();
     var sum = objCalculator.sumNumbers(4,5);
     expect(sum).toBe(9);
   });
 
   it("Summing multiply numbers", () => {
-    
-    // const objCalculator = new cCalculator();
     var sum = objCalculator.sumNumbers(1,2,3,4);
     expect(sum).toBe(10);
-
   });
 
   it("multiple two numbers", () => {
