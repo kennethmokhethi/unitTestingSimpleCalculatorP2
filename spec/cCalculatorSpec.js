698cf8ed769ef30b34f45b77c593558cae300156
KennethMokhethi <kennethmahlatsemokhethi@gmail.com>
2019-12-09 08:42:32 +0200

    Enabled testing via command line

--- spec/cCalculatorSpec.js
+++ spec/cCalculatorSpec.js
@@ -1,48 +1,48 @@
+let objCalculator = require("../src/cCalculator");
 //Testing the functions for addintion of two and multiply numbers
 describe("Class:Addition operator,two arguments", () => {
   it("Summing up two numbers.test1", () => {
-    var sum = objCalculator.sumNumbers(0,0);
+    var sum = objCalculator.sumNumbers(0, 0);
     expect(sum).toBe(0);
   });
 
   it("Summing up two numbers.test2", () => {
-    var sum = objCalculator.sumNumbers(-1,-1);
+    var sum = objCalculator.sumNumbers(-1, -1);
     expect(sum).toBe(-2);
   });
 
   it("Summing up two numbers.test3", () => {
-    var sum = objCalculator.sumNumbers(4,5);
+    var sum = objCalculator.sumNumbers(4, 5);
     expect(sum).toBe(9);
   });
 
   it("Summing multiply numbers", () => {
-    var sum = objCalculator.sumNumbers(1,2,3,4);
+    var sum = objCalculator.sumNumbers(1, 2, 3, 4);
     expect(sum).toBe(10);
   });
 
   it("multiple two numbers", () => {
-    var product = objCalculator.multiplyMultNums(1,2);
+    var product = objCalculator.multiplyMultNums(1, 2);
     expect(product).toBe(2);
   });
 
   it("multiply multiple numbers", () => {
-    var product = objCalculator.multiplyMultNums(1,2,3,4);
+    var product = objCalculator.multiplyMultNums(1, 2, 3, 4);
     expect(product).toBe(24);
   });
-   
-it("should return the last number",()=>{
-let lastN=objCalculator.last();
-expect(lastN).toBe(24); //from multiple spec that starts at line 40
-});
 
-it("Should set the slot",()=>{
- let setSlot=objCalculator.set_slot(objCalculator.lastNumber);
- expect(setSlot).toEqual([24]); //from multiple spec that starts at line 40
- });
+  it("should return the last number", () => {
+    let lastN = objCalculator.last();
+    expect(lastN).toBe(24); //from multiple spec that starts at line 40
+  });
 
-it("Should return the slot by index",()=>{
-let slot=objCalculator.get_slot(0);
-expect(slot).toBe(24);
-});
+  it("Should set the slot", () => {
+    let setSlot = objCalculator.set_slot(objCalculator.lastNumber);
+    expect(setSlot).toEqual([24]); //from multiple spec that starts at line 40
+  });
 
+  it("Should return the slot by index", () => {
+    let slot = objCalculator.get_slot(0);
+    expect(slot).toBe(24);
+  });
 });
--- src/cCalculator.js
+++ src/cCalculator.js
@@ -1,55 +1,47 @@
-class cCalculator{
-
- constructor(){
-  
-  this.arr=[];
-  this.lastNumber=0;
- }
-
-// Sum up multiple numbers 
-sumNumbers()
-{
-    var sum=0;
-    for(let i=0;i<arguments.length;i++)
-    {
-        sum+=arguments[i];
-    }
-  this.lastNumber=sum;
-    return sum;
-}
-
-// functions that multipl multiple numbers together
-multiplyMultNums()
-{
-  var product=1;
-  for(let i=0;i<arguments.length;i++)
-  {
-      product=product*arguments[i];
+class cCalculator {
+  constructor() {
+    this.arr = [];
+    this.lastNumber = 0;
   }
-  this.lastNumber=product;
-  return product;
-}
 
-//returns the last number
-last(){
-  return this.lastNumber;
-}
+  // Sum up multiple numbers
+  sumNumbers() {
+    var sum = 0;
+    for (let i = 0; i < arguments.length; i++) {
+      sum += arguments[i];
+    }
+    this.lastNumber = sum;
+    return sum;
+  }
 
-//setting the slot memory
-set_slot(x){
-  // let count=0;
-  this.arr.push(x);
-  // count++;
-  return this.arr;
-}
+  // functions that multipl multiple numbers together
+  multiplyMultNums() {
+    var product = 1;
+    for (let i = 0; i < arguments.length; i++) {
+      product = product * arguments[i];
+    }
+    this.lastNumber = product;
+    return product;
+  }
 
-//return the slot
-get_slot(x){
-  
-  return this.arr[x];
+  //returns the last number
+  last() {
+    return this.lastNumber;
+  }
 
-}
+  //setting the slot memory
+  set_slot(x) {
+    // let count=0;
+    this.arr.push(x);
+    // count++;
+    return this.arr;
+  }
 
+  //return the slot
+  get_slot(x) {
+    return this.arr[x];
+  }
 }
 
 let objCalculator = new cCalculator();
+module.exports = objCalculator;
